4a0eb98ede04b1d7b54df6e1c526abe8f5f69cef
Soeun21 <131752096+Soeun21@users.noreply.github.com>
2023-11-29 15:25:53 +0900

    day03-실전예제 연습

---
+++ Day03-jpabook/src/main/java/JPAMain.java
@@ -0,0 +1,30 @@
+import ex01.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JPAMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+
+            em.persist(book);
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
