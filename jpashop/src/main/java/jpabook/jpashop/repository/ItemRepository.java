aea8dfa125ffd973261fcd1945afa836a2f882c7
Soeun21 <131752096+Soeun21@users.noreply.github.com>
2023-11-30 17:34:45 +0900

    day03 - 웹계층 개발, 변경감지와 merge, 조회, 수정, 취소

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -20,7 +20,10 @@ public class ItemRepository {
         if(item.getId() == null){   // id 값이 없다면 새로 생성한 객체를 의미
             em.persist(item);   // 새로 생성한 객체를 신규로 등록
         } else {    // 값이 있다면 디비에 등록된 값을 가져온 것
-            em.merge(item); // 대충 업데이트를 의미
+            em.merge(item);
+            /*변경 감지 기능을 사용하면 원하는 속성만 선택해서 변경할 수 있지만,
+            병합을 사용하면 모든 속성이 변경된다.
+            병합시 값이 없으면 null 로 업데이트 할 위험도 있다. (병합은 모든 필드를 교체한다.)*/
         }
     }
 
